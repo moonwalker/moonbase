ddec0b10ca86789c70cdee8278e930fe43aa89e3
Kalman Speier <kalman.speier@gmail.com>
2022-11-24 17:26:48 +0100

    gh login for docs

--- internal/api/docs.go
+++ internal/api/docs.go
@@ -20,21 +20,34 @@ const (
 	loginBtnHtml = `<button id="loginbtn" class="btn" style="margin-right: 1em">Login</button>`
 )
 
+const (
+	jsPopupFunc = "function popupWindow(url, windowName, win, w, h) { const y = win.top.outerHeight / 2 + win.top.screenY - ( h / 2); const x = win.top.outerWidth / 2 + win.top.screenX - ( w / 2); return win.open(url, windowName, `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`);}"
+)
+
 func docsHandler() http.HandlerFunc {
 	d.SwaggerInfo.Description += fmt.Sprintf("\nRevision: ```%s```", runtime.ShortRev())
 	return httpSwagger.Handler(
+		httpSwagger.BeforeScript(jsPopupFunc),
 		httpSwagger.UIConfig(map[string]string{
 			"persistAuthorization": "true",
 			"requestInterceptor": `(req) => {
 				if (req.headers['Authorization']) {
-					req.headers['Authorization'] = 'Bearer ' + req.headers['Authorization']
+					if (!req.headers['Authorization'].startsWith('Bearer')) {
+						req.headers['Authorization'] = 'Bearer ' + req.headers['Authorization']
+					}
 				}
 				return req
 			}`,
 			"onComplete": fmt.Sprintf(`() => {
 				document.querySelector(".auth-wrapper").insertAdjacentHTML("afterbegin", '%s')
 				document.querySelector("#loginbtn").addEventListener('click', () => {
-					window.open("/login/github?returnURL=/login/github/authenticate")
+					const authwin = popupWindow("/login/github?returnURL=/login/github/authenticate", "swagger-gh-auth", window, 640, 480)
+					authwin.onload = () => {
+						const res = JSON.parse(authwin.document.querySelector("pre").innerText)
+						window.localStorage.setItem('authorized', JSON.stringify({"bearerToken":{"name":"bearerToken","schema":{"type":"apiKey","name":"Authorization","in":"header"},"value":res.token}}))
+						authwin.close()
+						window.location.reload()
+					}
 				})
 			}`, loginBtnHtml),
 		}),
