83908d98dee760c9e4a3aa9b1d000d742c6e8487
rucsi <rucsi6@gmail.com>
2023-08-24 10:48:36 -0500

    clear existing locale values

--- internal/cms/merge.go
+++ internal/cms/merge.go
@@ -23,7 +23,6 @@ func MergeLocalisedContent(rc []*github.RepositoryContent, cs content.Schema) (*
 	result := &content.MergedContentData{}
 	result.Fields = make(map[string]map[string]interface{})
 
-	// defaultFieldValues := make(map[string]interface{})
 	localizedFields := make(map[string]bool)
 
 	//Build Localized field bool from schema
@@ -52,28 +51,41 @@ func MergeLocalisedContent(rc []*github.RepositoryContent, cs content.Schema) (*
 				if result.Fields[k] == nil {
 					result.Fields[k] = make(map[string]interface{})
 				}
-				// Check if field is localized
-				if l == content.DefaultLocale || localizedFields[k] {
+
+				if l == content.DefaultLocale {
+					result.Name = n
+					result.Type = *c.Type
+
+					result.ID = dcd.ID
+					if dcd.CreatedAt != "" {
+						ct, _ := time.Parse(time.RFC3339, dcd.CreatedAt)
+						result.CreatedAt = &ct
+					}
+					result.CreatedBy = dcd.CreatedBy
+					if dcd.UpdatedAt != "" {
+						ut, _ := time.Parse(time.RFC3339, dcd.UpdatedAt)
+						result.UpdatedAt = &ut
+					}
+					result.UpdatedBy = dcd.UpdatedBy
+					result.Version = dcd.Version
+
 					result.Fields[k][l] = v
+					if localizedFields[k] && len(result.Fields[k]) > 0 {
+						// clear existing locale values if they are the same as the default if any
+						for el, ev := range result.Fields[k] {
+							if el != content.DefaultLocale && ev == v {
+								result.Fields[k][el] = nil
+							}
+						}
+					}
+				} else if localizedFields[k] {
+					// check if field is localized and locale value is not the same as the default
+					if v != result.Fields[k][content.DefaultLocale] {
+						result.Fields[k][l] = v
+					} else {
+						result.Fields[k][l] = nil
+					}
 				}
-			}
-
-			if content.DefaultLocale == l {
-				result.Name = n
-				result.Type = *c.Type
-
-				result.ID = dcd.ID
-				if dcd.CreatedAt != "" {
-					ct, _ := time.Parse(time.RFC3339, dcd.CreatedAt)
-					result.CreatedAt = &ct
-				}
-				result.CreatedBy = dcd.CreatedBy
-				if dcd.UpdatedAt != "" {
-					ut, _ := time.Parse(time.RFC3339, dcd.UpdatedAt)
-					result.UpdatedAt = &ut
-				}
-				result.UpdatedBy = dcd.UpdatedBy
-				result.Version = dcd.Version
 			}
 		}
 	}
